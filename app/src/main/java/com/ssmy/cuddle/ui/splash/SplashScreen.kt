a4f3c15f66f033c73f81949c5b59cc74a6c63a3e
wookjin <jin090130@webcash.co.kr>
2024-07-05 15:58:23 +0900

    필요한 액티비티 생성

---
+++ app/src/main/java/com/ssmy/cuddle/ui/splash/SplashScreen.kt
@@ -0,0 +1,43 @@
+package com.ssmy.cuddle.ui.splash
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.ssmy.cuddle.ui.splash.ui.theme.CuddleandroidTheme
+
+class SplashScreen : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CuddleandroidTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    Greeting2("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting2(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview2() {
+    CuddleandroidTheme {
+        Greeting2("Android")
+    }
+}
