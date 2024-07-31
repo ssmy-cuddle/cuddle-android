2643772fc3ec14123986f6a47b5b0bdc303f20ac
wookjin <jin090130@webcash.co.kr>
2024-07-31 10:39:47 +0900

    Apply .gitignore

---
+++ app/src/main/java/com/ssmy/cuddle/MyApp.kt
@@ -0,0 +1,9 @@
+package com.ssmy.cuddle
+
+/**
+ * doc 주석
+ * @author wookjin
+ * @since 7/31/24
+ **/
+class MyApp {
+}
---
+++ app/src/main/java/com/ssmy/cuddle/ui/di/AppModule.kt
@@ -0,0 +1,9 @@
+package com.ssmy.cuddle.ui.di
+
+/**
+ * doc 주석
+ * @author wookjin
+ * @since 7/31/24
+ **/
+class AppModule {
+}
