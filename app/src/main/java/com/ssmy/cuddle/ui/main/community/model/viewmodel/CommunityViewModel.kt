668c78f12f2ebf24aa5f35114d93eca75f07a792
wookjin <wookoding@gmail.com>
2024-08-17 23:46:41 +0900

    커뮤니티 화면 구성

---
+++ app/src/main/java/com/ssmy/cuddle/ui/main/community/model/viewmodel/CommunityViewModel.kt
@@ -0,0 +1,22 @@
+package com.ssmy.cuddle.ui.main.community.model.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class CommunityViewModel : ViewModel() {
+
+    private val _location = MutableLiveData("상도동")
+    val location: LiveData<String> = _location
+
+    fun updateLocation(newLocation: String) {
+        _location.value = newLocation
+    }
+
+    private val _selectedTab = MutableLiveData(0)
+    val selectedTab: LiveData<Int> = _selectedTab
+
+    fun selectTab(index: Int) {
+        _selectedTab.value = index
+    }
+}
