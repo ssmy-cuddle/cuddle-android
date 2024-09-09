e44bea9736df98aaef3abe6459fcc455a638fab7
wookjin <jin090130@webcash.co.kr>
2024-09-09 16:54:52 +0900

    동물 프로필 개발 중

---
+++ app/src/main/java/com/ssmy/cuddle/ui/main/contents/profile/view/activitys/AnimalProfileActivity.kt
@@ -0,0 +1,12 @@
+package com.ssmy.cuddle.ui.main.contents.profile.view.activitys
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ssmy.cuddle.R
+
+class AnimalProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_animal_profile)
+    }
+}
