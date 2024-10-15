81385b75ed4fd7a4948442d9f4cb86415d921c17
wookjin <jin090130@webcash.co.kr>
2024-10-15 16:40:34 +0900

    동물 프로필 화면 구성

--- app/src/main/java/com/ssmy/cuddle/ui/main/contents/profile/view/activitys/AnimalProfileActivity.kt
+++ app/src/main/java/com/ssmy/cuddle/ui/main/contents/profile/view/activitys/AnimalProfileActivity.kt
@@ -1,12 +1,20 @@
 package com.ssmy.cuddle.ui.main.contents.profile.view.activitys
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.databinding.DataBindingUtil
 import com.ssmy.cuddle.R
+import com.ssmy.cuddle.databinding.ActivityAnimalProfileBinding
+import com.ssmy.cuddle.ui.base.BaseActivity
+import com.ssmy.cuddle.ui.main.contents.profile.viewmodels.AnimalProfileViewModel
 
-class AnimalProfileActivity : AppCompatActivity() {
+class AnimalProfileActivity : BaseActivity<AnimalProfileViewModel>() {
+
+    override val viewModel: AnimalProfileViewModel by viewModels()
+    private lateinit var binding: ActivityAnimalProfileBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_animal_profile)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_animal_profile)
     }
 }
