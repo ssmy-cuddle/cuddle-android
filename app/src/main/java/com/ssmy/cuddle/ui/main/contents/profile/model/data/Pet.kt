53c9e5a0ae6517ea2bdd1d5c679d30a08589a1c1
wookjin <jin090130@webcash.co.kr>
2024-10-22 10:36:18 +0900

    프로필 ui

--- app/src/main/java/com/ssmy/cuddle/ui/main/contents/profile/model/data/Pet.kt
+++ app/src/main/java/com/ssmy/cuddle/ui/main/contents/profile/model/data/Pet.kt
@@ -8,12 +8,12 @@ package com.ssmy.cuddle.ui.main.contents.profile.model.data
 data class Pet(
     val id: Int,
     val name: String,
-    val imageUrl: String,
+//    val imageUrl: String,
     val gender: Int,
     val breed: String,
     val birthday: String,
     val weight: String,
     val isNeutered: Boolean,
     val daysTogether: String,
-    val isDeceased: Boolean
+//    val isDeceased: Boolean
 )
